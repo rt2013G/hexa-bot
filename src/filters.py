3c4dc5d0f68e5dda35cb698e1ad51fa3a562445f
rt2013G <talente63@gmail.com>
2024-02-27 16:56:13 +0100

    some minor tweaks

--- src/filters.py
+++ src/filters.py
@@ -1,6 +1,7 @@
 from telegram import Message
 from telegram.ext.filters import MessageFilter
-from src.utils.utils import is_admin, is_market_group, is_main_group, is_approval_group
+from src.utils.utils import is_admin
+from src.utils.config import get_main_id, get_market_id, get_approval_id
 
 class AdminFilter(MessageFilter):
     def filter(self, message: Message) -> bool:
@@ -8,13 +9,12 @@ class AdminFilter(MessageFilter):
     
 class MarketGroupFilter(MessageFilter):
     def filter(self, message: Message) -> bool:
-        return is_market_group(message.chat.id)
+        return message.chat.id == get_market_id()
     
 class MainGroupFilter(MessageFilter):
     def filter(self, message: Message) -> bool:
-        return is_main_group(message.chat.id)
+        return message.chat.id == get_main_id()
     
 class ApprovalGroupFilter(MessageFilter):
     def filter(self, message: Message) -> bool:
-        return is_approval_group(message.chat.id)
-
+        return message.chat.id == get_approval_id()
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,13 +1,7 @@
-from src.utils.config import get_market_id, get_main_id, get_approval_id
+from src.database.dbms import get_role_list
 
 def is_admin(user_id) -> bool:
-    pass
-
-def is_market_group(chat_id) -> bool:
-    return chat_id == get_market_id()
-
-def is_main_group(chat_id) -> bool:
-    return chat_id == get_main_id()
-
-def is_approval_group(chat_id) -> bool:
-    return chat_id == get_approval_id
+    id_list = []
+    for user in get_role_list("admin"):
+        id_list.append(user.id)
+    return user_id in id_list
